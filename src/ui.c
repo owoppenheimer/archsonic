b8b575112dc12206c793a51e27325ca9f8dae3d2
owoppenheimer <naturalotvet@gmail.com>
2025-06-06 11:33:49 +0500

    ui.c: optimization

--- src/ui.c
+++ src/ui.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include "../include/archsonic_types.h"
 
 struct {
     unsigned int height, width;
@@ -11,8 +12,6 @@ struct {
 void geterr(const char* error) {
     fprintf(stderr, "Ошибка: %s\n", error);
 }
-
-typedef struct { int y, x; } term_point;
 
 int main(void) {
     char* str_height = malloc(32 * sizeof(char));
@@ -31,34 +30,21 @@ int main(void) {
     setlocale(LC_ALL, "");
     initscr();
     noecho();
-    keypad(stdscr, TRUE);
 
-    getmaxyx(stdscr, window_size.height, window_size.width);
-    term_point point = { window_size.height / 2, window_size.width / 2 };
-
-    sprintf(str_height + strlen("Высота: "), "%d", window_size.height);
-    sprintf(str_width + strlen("Ширина: "), "%d", window_size.width);
-
-    mvprintw(point.y - 1, point.x - strlen(str_width)/2, "%s", str_width);
-    mvprintw(point.y, point.x - strlen(str_height)/2, "%s", str_height);
     refresh();
 
     while (1) {
+        getmaxyx(stdscr, window_size.height, window_size.width);
+        term_point point = { window_size.height / 2, window_size.width / 2 };
+
+        clear();
+
+        sprintf(str_height + strlen("Высота: "), "%d", window_size.height);
+        sprintf(str_width + strlen("Ширина: "), "%d", window_size.width);
+        mvprintw(point.y - 1, point.x - strlen(str_width)/2, "%s", str_width);
+        mvprintw(point.y, point.x - strlen(str_height)/2, "%s", str_height);
         int c = getch();
-        if (c == 27) break;  // ESC
-        if (c == KEY_RESIZE) {
-            getmaxyx(stdscr, window_size.height, window_size.width);
-            point.y = window_size.height / 2;
-            point.x = window_size.width / 2;
-
-            clear();
-
-            sprintf(str_height + strlen("Высота: "), "%d", window_size.height);
-            sprintf(str_width + strlen("Ширина: "), "%d", window_size.width);
-            mvprintw(point.y - 1, point.x - strlen(str_width)/2, "%s", str_width);
-            mvprintw(point.y, point.x - strlen(str_height)/2, "%s", str_height);
-        }
-        refresh();
+        if (c == 27) { break; refresh(); }  
     }
 
     free(str_height);
