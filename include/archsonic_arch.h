81f7a789446bf370aa93c5c24bc63e8645f4be0f
owoppenheimer <naturalotvet@gmail.com>
2025-06-06 20:53:34 +0500

    archsonic_arch.h: functions and other stuff to install arch

---
+++ include/archsonic_arch.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef ARCHSONIC_ARCH
+#define ARCHSONIC_ARCH
+
+typedef struct arch_packages_c {
+
+   int packages_c;
+   int excluded_packages_c;
+
+} arch_packages_c;
+
+struct arch_packages {
+
+   char** packages;
+   int capacity;
+   int count;
+
+};
+
+void geterror(unsigned short error_code, const char* error_description);
+
+void init_install(struct arch_packages* ap, int init_capacity);
+
+void add_package(struct arch_packages* ap, const char* name);
+
+void free_package_list(struct arch_packages* ap);
+
+void rm_package(struct arch_packages* ap, const char* name);
+
+static unsigned char arch_installation_status;
+
+void arch_install(int packages_c, int excluded_packages_c, struct arch_packages* packages);
+
+#endif
