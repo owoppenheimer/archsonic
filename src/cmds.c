1ec62e13a3c772c0cd14e224c4da3a60a8f04245
owoppenheimer <naturalotvet@gmail.com>
2025-06-08 13:58:04 +0500

    cmds.c: fixes

--- src/cmds.c
+++ src/cmds.c
@@ -1,36 +1,23 @@
 #include "../include/archsonic_commands.h"
-#include "../include/archsonic_arch.h"
 #include <stdlib.h>
 
-const char* pre_commands  = {
-   "genfstab /"
+const char* pre_commands[]  = {
+   "echo sixty six dicks in my ass" // wha?
 };
-const char* post_commands = {
+const char* post_commands[] = {
    "echo suka"
 };
 
-void exec_pre_cmds(const char* pre_commands) {
-   /* Check the pointer */
-   if (!pre_commands) {
-      geterror(1, "Couldn't find commands!");  
-      return;
-   }
-
+void exec_pre_cmds(void) {
    /* Execute the commands from array */
-   for (int i = 0; i < sizeof(*pre_commands) / sizeof(pre_commands[0]); i++) { 
-      system(&pre_commands[i]);      
+   for (int i = 0; i < sizeof(pre_commands) / sizeof(pre_commands[0]); i++) { 
+      system(pre_commands[i]);      
    }
 }
 
-void exec_post_cmds(const char *post_commands) {
-   /* Check the pointer */
-   if (!post_commands) {
-      geterror(1, "Couldn't find commands!");
-      return;
-   }
-
+void exec_post_cmds(void) {
    /* Execute the commands from array */
-   for (int i = 0; i < sizeof(*post_commands) / sizeof(post_commands[0]); i++) {
-      system(&post_commands[i]);
+   for (int i = 0; i < sizeof(post_commands) / sizeof(post_commands[0]); i++) {
+      system(post_commands[i]);
    }
 }
