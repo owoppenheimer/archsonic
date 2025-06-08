6128b5ef795093c0ffac2f6ea323731e81fbd37c
owoppenheimer <owoppenheimer@gmail.com>
2025-06-08 18:53:27 +0500

    Update archsonic.c

--- src/archsonic.c
+++ src/archsonic.c
@@ -1,8 +1,8 @@
 #include "../include/archsonic_commands.h"
 
 int main(void) {
-   exec_pre_cmds(pre_commands);
-   exec_post_cmds(post_commands);
+   exec_pre_cmds();
+   exec_post_cmds();
 
    return 0;
 }
