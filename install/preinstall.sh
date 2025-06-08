cd3c072ce9066db6bfdfe48d2944fdcca27e7ba1
owoppenheimer <naturalotvet@gmail.com>
2025-06-08 19:12:20 +0500

    preinstall.sh: fixed

--- install/preinstall.sh
+++ install/preinstall.sh
@@ -1,9 +1,7 @@
 main() {
    curl -fsSL https://raw.githubusercontent.com/owoppenheimer/archsonic/refs/heads/arch/install/install.sh > ./install.sh &
    chmod +x ./install.sh
-
-   # exectute the script
-   su -c "./install.sh"
+   echo "Ok, now just execute installer with './install.sh'"
 }
 
 main
