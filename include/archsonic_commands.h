4defe11a7c0ce1e59684ac8f1f3d53ac2cad0af8
owoppenheimer <naturalotvet@gmail.com>
2025-06-08 13:57:32 +0500

    archsonic_commands.h: now they're an arrays

--- include/archsonic_commands.h
+++ include/archsonic_commands.h
@@ -3,10 +3,10 @@
 #ifndef ARCHSONIC_COMMANDS
 #define ARCHSONIC_COMMANDS
 
-extern const char* pre_commands;
-extern const char* post_commands;
+extern const char* pre_commands[];
+extern const char* post_commands[];
 
-void exec_pre_cmds(const char* pre_commands);
-void exec_post_cmds(const char* post_commands);
+void exec_pre_cmds(void);
+void exec_post_cmds(void);
 
 #endif
