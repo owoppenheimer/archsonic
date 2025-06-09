ed05d088536aa2e95d50ab9705d724ca2a2c7295
owoppenheimer <naturalotvet@gmail.com>
2025-06-09 15:52:00 +0500

    archsonic_commands.h: refactoring

--- include/archsonic_commands.h
+++ include/archsonic_commands.h
@@ -3,10 +3,9 @@
 #ifndef ARCHSONIC_COMMANDS
 #define ARCHSONIC_COMMANDS
 
-extern const char* pre_commands[];
-extern const char* post_commands[];
+void exec_cmds(char* cmds[]);
 
-void exec_pre_cmds(void);
-void exec_post_cmds(void);
+void add_user(const char* username);
+void config_passwds(const char* passwd, const char* mode, const char* username);
 
 #endif
